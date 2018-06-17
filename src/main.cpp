4c7ab049e04a5023bd9151e30936e96b2677f424
Christopher Dyken <cdyk@users.noreply.github.com>
2018-06-17 16:07:53 +0200

    Inital skeleton. Added stb_image

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include "stb_image_write.h"
+#include <cstdint>
+#include <cstdio>
+
+int main(int argc, char** argv)
+{
+  const char* filename = "output.png";
+  const unsigned w = 200;
+  const unsigned h = 100;
+
+  uint8_t image[3 * w * h];
+
+  for (unsigned j = 0; j < h; j++) {
+    for (unsigned i = 0; i < w; i++) {
+      image[3 * (j*w + i) + 0] = i;
+      image[3 * (j*w + i) + 1] = j;
+      image[3 * (j*w + i) + 2] = 0;
+    }
+  }
+
+  if (stbi_write_png(filename, w, h, 3, image, 3*w)) {
+    fprintf(stderr, "Failed to write png file '%s'", filename);
+    return -1;
+  }
+
+  return 0;
+}
