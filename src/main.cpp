b2eb13a049963c1946e5b12cc3d3b7d65da1dbde
Christopher Dyken <cdyk@users.noreply.github.com>
2018-06-18 22:13:07 +0200

    diffuse

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,24 @@ namespace {
     return (1.f / (1.f + RAND_MAX))*float(rand());
   }
 
+  vec3 random_in_unit_sphere()
+  {
+    vec3 p;
+    do {
+      p = 2.f*vec3(frand(), frand(), frand()) - vec3(1, 1, 1);
+    } while (dot(p, p) >= 1.f);
+    return p;
+  }
 
-  vec3 color(const ray& r, intersectable* world)
+  vec3 color(const ray& r, intersectable* world, unsigned depth)
   {
     intersection hit;
 
-    if (world->intersect(r, 0.f, std::numeric_limits<float>::max(), hit)) {
-      return 0.5f*(hit.n + vec3(1.f, 1.f, 1.f));
+    if (depth &&  world->intersect(r, 0.001f, std::numeric_limits<float>::max(), hit)) {
+
+      vec3 target = hit.p + hit.n + random_in_unit_sphere();
+      return 0.5f*color(ray(hit.p, target - hit.p), world, depth - 1);
+      //return 0.5f*(hit.n + vec3(1.f, 1.f, 1.f));
     }
 
     vec3 v = normalize(r.dir);
@@ -66,13 +77,14 @@ int main(int argc, char** argv)
 
         auto r = cam.getRay(u, v);
 
-        col = col + color(r, world);
+        col = col + color(r, world, 20);
       }
       col = (1.f / s)*col;
+      
 
-      image[3 * (j*w + i) + 0] = uint8_t(255.f*saturate(col.r));
-      image[3 * (j*w + i) + 1] = uint8_t(255.f*saturate(col.g));
-      image[3 * (j*w + i) + 2] = uint8_t(255.f*saturate(col.b));
+      image[3 * (j*w + i) + 0] = uint8_t(255.f*saturate(std::sqrt(col.r)));
+      image[3 * (j*w + i) + 1] = uint8_t(255.f*saturate(std::sqrt(col.g)));
+      image[3 * (j*w + i) + 2] = uint8_t(255.f*saturate(std::sqrt(col.b)));
     }
   }
 
